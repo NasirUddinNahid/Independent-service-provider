35aec51dcc80c17c78026088f34837f108e3e83f
NasirUddinNahid <nahidahmed17762@gmail.com>
2022-04-17 21:43:43 +0600

    extrapart-styling

--- src/Pages/Home/Extra-Section/Extra.js
+++ src/Pages/Home/Extra-Section/Extra.js
@@ -4,17 +4,17 @@ import './Extra.css'
 
 const Extra = () => {
     return (
-        <div>
-            <div className='fixing-extra-part'>
+        <div className='fixing-extra-part'>
+            <div>
                 <h1>Get In Touch</h1>
                 <p>
-                If you have a question or <br />
-                any issues in my gym I <br />
-                would love to help Badly.
+                    If you have a question or <br />
+                    any issues in my gym I <br />
+                    would love to help Badly.
                 </p>
             </div>
-            <div>
-                <img style={{width:'650px', height:'550px'}} src={extrasectionImg} alt="" />
+            <div className='extra-img'>
+                <img src={extrasectionImg} alt="" />
             </div>
         </div>
     );
